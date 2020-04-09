27d4574827bd916069b06081308ae2b51178cfce
Ngoc <ngocsonez023@gmail.com>
2020-04-09 14:47:34 +0700

    add something

--- app/Http/Controllers/restFullAPIController.php
+++ app/Http/Controllers/restFullAPIController.php
@@ -13,11 +13,24 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 
 class restFullAPIController extends Controller
 {
+  public function __construct()
+    {
+      // We set the guard api as default driver
+      
+      // auth()->setDefaultDriver('admin');
+
+      //reference: https://medium.com/@akgarg007/laravel-6-jwt-multi-model-authentication-d5b416c4cdb4
+    }
    // reference : https://viblo.asia/p/jwt-json-web-tokens-trong-laravel-57-1VgZvoaOlAw
    // https://viblo.asia/p/api-authentication-su-dung-jwt-trong-laravel-1VgZvxRY5Aw
+  // https://codingpearls.com/huong-dan-lap-trinh-restful-api-voi-laravel-5
+  // https://www.youtube.com/watch?v=5kmr55zUcQM
+
+    // reference for Authorization = null
+    // https://github.com/laravel/framework/issues/6482
 
     public function register(Request $request){
-        $user = DB::table('tbluserapi')->insert([
+        $user = DB::table('users')->insert([
           'name' => $request->get('name'),
           'email' => $request->get('email'),
           'password' => Hash::make($request->get('password'))
@@ -46,8 +59,29 @@ class restFullAPIController extends Controller
         return response()->json(compact('token'));
     }
 
+    public function adminlogin(Request $request){
+        // Config::set('jwt.user', "App\Admin");
+        // Config::set('auth.providers.admins.model', \App\Admin::class);
+
+        auth()->setDefaultDriver('admin');
+        $credentials = $request->only('email', 'password');
+        
+        $token = null;
+
+        try {
+            
+           if (!$token = JWTAuth::attempt($credentials)) {
+            
+            return response()->json(['invalid_email_or_password'], 422);
+           }
+        } catch (JWTAuthException $e) {
+            return response()->json(['failed_to_create_token'], 500);
+        }
+        return response()->json(compact('token'));
+    }
+
     public function getUserInfo(Request $request){
-        $user = JWTAuth::toUser($request->token);
-        return response()->json(['result' => $user]);
+      $user = JWTAuth::toUser($request->token);
+      return response()->json(['result' => $user]);
     }
 }
